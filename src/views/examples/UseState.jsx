ddf70f93f13d6ec9abc3733597ca44b487661d8a
AlbertoParente <albertoparente20@gmail.com>
2021-03-23 23:16:13 -0300

    Adding the useState concept

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
+    const [count, setCount] = useState(0)
+    
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="State in functional components!"
             />
+            <SectionTitle title="Activity #01" />
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn"
+                        onClick={() => setCount(current => count - 1000)}>-1000</button>
+                    <button className="btn"
+                        onClick={() => setCount(count - 1)}>-1</button>
+                    <button className="btn"
+                        onClick={() => setCount(count + 1)}>+1</button>
+                    <button className="btn"
+                        onClick={() => setCount(current => count + 1000)}>+1000</button>
+                </div>
+            </div>
+            
+            <SectionTitle title="Activity #02" />
         </div>
     )
 }
