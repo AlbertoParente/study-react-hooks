4a89a5330d295f6d7379e73eadbc985d71685d41
AlbertoParente <albertoparente20@gmail.com>
2021-03-24 22:11:45 -0300

    Adding the useEffect concept

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+function calcFatorial(num) {
+    const n = parseInt(num)
+
+    if(n < 0) return -1
+    if(n === 0) return 1
+    return calcFatorial(n - 1) * n
+}
+
 const UseEffect = (props) => {
+    const [number, setNumber] = useEffect(1)
+    const [fatorial, setFatorial] = useState(1)
+
+    useEffect(function() {
+        setFatorial(calcFatorial(number))
+    }, [number])
+
+    useEffect(function() {
+        if(fatorial > 1000000) {
+            document.title = "Wow!!!"
+        }
+    }, [fatorial])
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Allows you to perform side effects on functional components!"
             />
+            <SectionTitle title="Activity #01" />
+            <div className="center">
+                <div>
+                    <span className="text">Fatorial: </span>
+                    <span className="text red">{fatorial === -1 ? 'Not Exists' : fatorial}</span>
+                </div>
+                <input type="number" className="input"
+                    value={number}
+                    onChange={e => setNumber(e.target.value)}/>
+            </div>
         </div>
     )
 }
