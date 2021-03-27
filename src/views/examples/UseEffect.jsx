799397b67943e7835bcc968fda9adac972ab1354
AlbertoParente <albertoparente20@gmail.com>
2021-03-26 23:39:29 -0300

    Bugfix useEffect

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,34 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 function calcFatorial(num) {
     const n = parseInt(num)
 
-    if(n < 0) return -1
-    if(n === 0) return 1
+    if (n < 0) return -1
+    if (n === 0) return 1
     return calcFatorial(n - 1) * n
 }
 
 const UseEffect = (props) => {
 
     // Ex.: #01
-    const [number, setNumber] = useEffect(1)
+    const [number, setNumber] = useState(1)
     const [fatorial, setFatorial] = useState(1)
 
-    useEffect(function() {
+    useEffect(function () {
         setFatorial(calcFatorial(number))
     }, [number])
 
-    useEffect(function() {
-        if(fatorial > 1000000) {
+    useEffect(function () {
+        if (fatorial > 1000000) {
             document.title = "Wow!!!"
         }
     }, [fatorial])
 
-    // Ex.: #01
+    // Ex.: #02
     const [status, setStatus] = useState("ODD")
 
-    useEffect(function() {
+    useEffect(function () {
         setStatus(number % 2 === 0 ? "Even" : "Odd")
     }, [number])
 
@@ -46,13 +47,15 @@ const UseEffect = (props) => {
                 </div>
                 <input type="number" className="input"
                     value={number}
-                    onChange={e => setNumber(e.target.value)}/>
+                    onChange={e => setNumber(e.target.value)} />
             </div>
 
             <SectionTitle title="Activity #02" />
             <div className="center">
-                <span className="text">Status: </span>
-                <span className="text red">{status}</span>
+                <div>
+                    <span className="text">Status: </span>
+                    <span className="text red">{status}</span>
+                </div>
             </div>
         </div>
     )
