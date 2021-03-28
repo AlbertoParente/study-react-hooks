d512b8b028138cbab3291fc0470ecb8180f47233
AlbertoParente <albertoparente20@gmail.com>
2021-03-27 23:09:26 -0300

    Adding the useRef concept

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseRef = (props) => {
+    const [value1, sedtValue1] = useState("") 
+    const count = useRef(0)
+
+    useEffect(function() {
+        count.current = count.current + 1
+    }, [value1])
+
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Returns a mutable object with the .current property!"
             />
+            <SectionTitle title="Activity #01" />
+            <div className="center">
+                <div>
+                    <span className="text">Valor: </span>
+                    <span className="text">{value1} [</span>
+                    <span className="text red">{count.current}</span>
+                    <span className="text">]</span>
+                </div>
+                <input type="text" className="input" 
+                    value={value1} onChange={e => set} />
+            </div>
         </div>
     )
 }
