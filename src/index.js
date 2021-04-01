82db4f1b1b0242a9911af92ca823544940d2bd33
AlbertoParente <albertoparente20@gmail.com>
2021-03-31 21:26:21 -0300

    Adding the useContext concept

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import './index.css'
 import App from './views/App'
+import DataContext, { data } from './data/DataContext'
 
 ReactDOM.render(
-    <App />,
+
+    <DataContext.Provider value={data}>
+        <App />
+    </DataContext.Provider>,
     document.getElementById('root')
 )
