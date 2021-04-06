f4db506b521a7268e1d80ba2df3c621d8caab3e0
AlbertoParente <albertoparente20@gmail.com>
2021-04-05 22:24:35 -0300

    Adding the useReduce concept

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+const initiaState = {
+    cart: [],
+    products: [],
+    user: [],
+    number: 0
+}
+
+function reduce(state, action) {
+    switch (action.type) {
+        case 'number_add2':
+            return {...state, number: state.number + 2}
+        default: 
+            return state
+    }
+}
+
 const UseReducer = (props) => {
+    const [state, exec] = useReducer(reduce, initiaState)
+
     return (
         <div className="UseReducer">
             <PageTitle
                 title="Hook UseReducer"
                 subtitle="Another way to have been in functional components!"
             />
+            <div className="center">
+                <span className="text">{state}</span>
+            </div>
         </div>
     )
 }
