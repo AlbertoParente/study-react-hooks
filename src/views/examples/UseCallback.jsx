9eafc54267e191e32cc132428404f88774581327
AlbertoParente <albertoparente20@gmail.com>
2021-04-09 22:26:13 -0300

    Removing test

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -21,25 +21,6 @@ const UseCallback = (props) => {
             </di>
         </div>
     )
-
-    const [count2, setCount2] = useState(0)
-
-    const inc = useCallback(function (delta) {
-        setCount2(current => current + delta)
-    }, [setCount2])
-
-    return (
-        <div className="UseCallback">
-            <PageTitle
-                title="Hook UseCallback"
-                subtitle="Returns a memoized function!"
-            />
-            <di className="center">
-                <span className="text">{count2}</span>
-                <UseCallbackButtons inc={inc} />
-            </di>
-        </div>
-    )
 }
 
 export default UseCallback
