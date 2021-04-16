12898caf163ed84dc9bed2aff6a8c4f4b95d261c
AlbertoParente <albertoparente20@gmail.com>
2021-04-15 22:57:37 -0300

    Adding the custom hooks concept, continuation

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import useCounter from '../../hooks/useCounter'
+import { useFetch } from '../../hooks/useFetch'
 
 const UseRef = (props) => {
     const [count, inc, dec] = useCounter()
+    const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+    const  response = useFetch(url)
+
+    function showStates(states) {
+        return states.map(state => <li key={state.nome}>{state.nome} - {state.sigla}</li>)
+    }
 
     return (
         <div className="UseCustom">
@@ -21,6 +28,12 @@ const UseRef = (props) => {
                     <button className="btn" 
                         onClick={() => dec()}>Dec</button>
                 </div>
+            </div>
+            <SectionTitle title="Activity #02" />
+            <div className="center">
+                <ul>
+                    {!response.loading ? showStates(response.data) : false}
+                </ul>
             </div>
         </div>
     )
